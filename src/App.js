1821b73841cfd83c0d5d78e4fdb229eef0027088
James Gary <gmarchesi888@gmail.com>
2018-12-22 10:09:08 -0500

    removed selection options for years not in DB.

--- src/App.js
+++ src/App.js
@@ -211,14 +211,12 @@ class ListForm extends React.Component {
               onChange={this.handleChange}
             >
               <option value="">Year</option>
-              <option value="2008">2008</option>
-              <option value="2009">2009</option>
-              <option value="2010">2010</option>
               <option value="2011">2011</option>
               <option value="2012">2012</option>
               <option value="2013">2013</option>
               <option value="2014">2014</option>
               <option value="2015">2015</option>
+              <option value="2015">2016</option>
             </select>
 
             <select
