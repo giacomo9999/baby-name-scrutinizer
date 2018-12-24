b44b03dff1bf4d1a079e96526d92b4c4ac206434
James Gary <gmarchesi888@gmail.com>
2018-12-24 16:45:02 -0500

    tweaks to fix DB typos are OK -- still not getting correct lists for some reason.

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
       )
       // extract relevant data from response object
       .then(response => {
-        console.log("Data retrieved from cityofnewyork.us. Building database...");
+        console.log(
+          "Data retrieved from cityofnewyork.us. Building database..."
+        );
         const recordObj = response.data.data.map((entry, index) => ({
           key: "entry_" + index,
           birthYear: entry[8],
@@ -27,18 +29,56 @@ class App extends Component {
           if (entry.race === "WHITE NON HISP") {
             entry.race = "WHITE NON HISPANIC";
           }
+          if (entry.race === "BLACK NON HISP") {
+            entry.race = "BLACK NON HISPANIC";
+          }
           if (entry.race === "ASIAN AND PACI") {
             entry.race = "ASIAN AND PACIFIC ISLANDER";
           }
         });
+
         // create a new state object without mutating the original one
         const newData = Object.assign({}, this.state, { namesData: recordObj });
-        
+
         // store the new object in the component's state
         this.setState(newData);
         console.log("Database assembled.");
 
-        console.log(this.state.namesData[55]);
+        // check validity of imported data
+        const raceArr = [
+          "BLACK NON HISPANIC",
+          "ASIAN AND PACIFIC ISLANDER",
+          "WHITE NON HISPANIC",
+          "HISPANIC"
+        ];
+        const yearsArr = ["2011", "2012", "2013", "2014", "2015", "2016"];
+        const sexArr = ["MALE", "FEMALE"];
+
+        for (let i = 0; i <= raceArr.length - 1; i++) {
+          for (let j = 0; j <= yearsArr.length - 1; j++) {
+            for (let k = 0; k <= sexArr.length - 1; k++) {
+              const filteredNamesData = recordObj.filter(
+                entry =>
+                  raceArr[i] === entry.race &&
+                  yearsArr[j] === entry.birthYear.toUpperCase() &&
+                  sexArr[k] === entry.sex.toUpperCase()
+              );
+
+              console.log(filteredNamesData[0]);
+
+              if (filteredNamesData.length === 0) {
+                console.log(
+                  raceArr[i],
+                  yearsArr[j],
+                  sexArr[k],
+                  " ** BAD DATA **"
+                );
+              } else {
+                console.log(raceArr[i], yearsArr[j], sexArr[k]);
+              }
+            }
+          }
+        }
       })
       .catch(error => console.log(error));
   }
