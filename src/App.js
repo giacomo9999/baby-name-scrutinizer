caa6b727e0337a9f3dde85d9d5e19f0d7397fe47
James Gary <gmarchesi888@gmail.com>
2018-12-24 09:52:56 -0500

    starting in on using real data.

--- src/App.js
+++ src/App.js
@@ -6,37 +6,37 @@ import axios from "axios";
 class App extends Component {
   state = { namesData: [] };
 
-  // componentDidMount() {
-  //   axios
-  //     .get(
-  //       "https://data.cityofnewyork.us/api/views/25th-nujf/rows.json?accessType=DOWNLOAD"
-  //     )
-  //     // extract relevant data from response object
-  //     .then(response => {
-  //       console.log("Data retrieved from cityofnewyork.us. Building database...");
-  //       const recordObj = response.data.data.map((entry, index) => ({
-  //         key: "entry_" + index,
-  //         birthYear: entry[8],
-  //         sex: entry[9],
-  //         race: entry[10],
-  //         name: entry[11],
-  //         withThatName: entry[12],
-  //         nameRank: entry[13]
-  //       }));
-  //       recordObj.forEach(entry => {
-  //         if (entry.race === "WHITE NON HISP") {
-  //           entry.race = "WHITE NON HISPANIC";
-  //         }
-  //       });
-  //       // create a new state object without mutating the original one
-  //       const newData = Object.assign({}, this.state, { namesData: recordObj });
+  componentDidMount() {
+    axios
+      .get(
+        "https://data.cityofnewyork.us/api/views/25th-nujf/rows.json?accessType=DOWNLOAD"
+      )
+      // extract relevant data from response object
+      .then(response => {
+        console.log("Data retrieved from cityofnewyork.us. Building database...");
+        const recordObj = response.data.data.map((entry, index) => ({
+          key: "entry_" + index,
+          birthYear: entry[8],
+          sex: entry[9],
+          race: entry[10],
+          name: entry[11],
+          withThatName: entry[12],
+          nameRank: entry[13]
+        }));
+        recordObj.forEach(entry => {
+          if (entry.race === "WHITE NON HISP") {
+            entry.race = "WHITE NON HISPANIC";
+          }
+        });
+        // create a new state object without mutating the original one
+        const newData = Object.assign({}, this.state, { namesData: recordObj });
         
-  //       // store the new object in the component's state
-  //       this.setState(newData);
-  //       console.log("Database assembled.");
-  //     })
-  //     .catch(error => console.log(error));
-  // }
+        // store the new object in the component's state
+        this.setState(newData);
+        console.log("Database assembled.");
+      })
+      .catch(error => console.log(error));
+  }
 
   render() {
     return (
